051458e37d16d0edef42f0f131e9b48f960e1535
Jack Wheeler <jack.wheeler@rogers.com>
2024-05-14 16:12:56 -0400

    Complete!

--- src/08-function-return-type-annotations.problem.ts
+++ src/08-function-return-type-annotations.problem.ts
@@ -17,8 +17,17 @@ interface Post {
  * How do we ensure that makeUser ALWAYS
  * returns a user?
  */
-const makeUser = () => {
-  return {};
+const makeUser = (): User => {
+  return {
+    id: 0,
+    firstName: "Jack",
+    lastName: "Wheeler",
+    role: "admin",
+    posts: [{
+      id: 0,
+      title: "Something"
+    }],
+  };
 };
 
 it("Should return a valid user", () => {
